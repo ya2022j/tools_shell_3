f19c6b28dfb8a90ca64583536196711978845061
ya2022j <62124198+ya2022j@users.noreply.github.com>
2023-05-17 16:14:45 +0900

    Create install_Redis_Commander.sh

---
+++ 8. install_Redis_Commander/install_Redis_Commander.sh
@@ -0,0 +1,17 @@
+
+
+
+# Redis Commander：Redis Commander 是另一个流行的 Redis 可视化管理工具，
+# 它是一个基于 Web 的管理界面。你可以在 CentOS 上安装和配置 Redis Commander，
+# 然后通过浏览器访问来管理 Redis 实例。
+
+# 安装方法：
+
+
+ npm install -g redis-commander;
+# 启动 Redis Commander：
+
+
+
+redis-commander;
+# 默认情况下，Redis Commander将在本地的 8081 端口上运行。你可以在浏览器中访问 http://<服务器IP>:8081 来打开 Redis Commander 的管理界面。
