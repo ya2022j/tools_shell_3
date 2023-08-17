ba19a7e3ec6a62a50c3d9d929aaeac474da03a2c
ya2022j <62124198+ya2022j@users.noreply.github.com>
2023-08-17 14:33:58 +0900

    Update compile_proto.py

--- 12.centos_install_proto/compile_proto.py
+++ 12.centos_install_proto/compile_proto.py
@@ -1,6 +1,5 @@
 
 
-
 # 介绍python获取命令行参数的方法：getopt模和argparse模块。
 import os
 import sys
@@ -15,41 +14,46 @@ def main(argv):
     :param argv:
     :return:
     """
-    protofile =""
+    protofile = ""
 
     try:
-        options, args = getopt.getopt(argv, "hp:", ['help',"protofile="])
+        options, args = getopt.getopt(argv, "hp:", ['help', "protofile="])
 
         for option, value in options:
             if option in ("-p", "--protofile"):
-                protofile  = value
+                protofile = value
 
 # gRPC  +  micro
 # protoc -I=$SRC_DIR --go_out=$DST_DIR $SRC_DIR/addressbook.proto
         absolutePath = os.getcwd()
-        protofilePath = os.path.join(absolutePath,protofile)
-        subprocess.call("export PATH=$PATH:/usr/local/go/bin;",shell=True)
-        subprocess.call("go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2",shell=True)
-        subprocess.call("go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28",shell=True)
-        subprocess.call("go install google.golang.org/protobuf/cmd/protoc-gen-go@latest",shell=True)
-        subprocess.call("go install github.com/go-micro/generator/cmd/protoc-gen-micro@latest",shell=True)
-
-
+        protofilePath = os.path.join(absolutePath, protofile)
+        subprocess.call("export PATH=$PATH:/usr/local/go/bin;", shell=True)
+        subprocess.call(
+            "go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2", shell=True)
+        subprocess.call(
+            "go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28", shell=True)
+        subprocess.call(
+            "go install google.golang.org/protobuf/cmd/protoc-gen-go@latest", shell=True)
+        subprocess.call(
+            "go install github.com/go-micro/generator/cmd/protoc-gen-micro@latest", shell=True)
+        subprocess.call(
+            "go install github.com/micro/micro/v3@latest", shell=True)
+        subprocess.call(
+            "go install go-micro.dev/v4@latest", shell=True)
         # --go_out: protoc-gen-go: Plugin failed with status code 1.の解決方法
-        subprocess.call("export GOPATH=$HOME/go",shell=True)
-        subprocess.call("PATH=$PATH:$GOPATH/bin",shell=True)
+        subprocess.call("export GOPATH=$HOME/go", shell=True)
+        subprocess.call("PATH=$PATH:$GOPATH/bin", shell=True)
         # subprocess.call('export PATH="$PATH:$(go env GOPATH)/bin')
 
         #compile_cmd  = " protoc -I={1} --go_out={1} {0}".format(protofilePath,absolutePath)
-        compile_cmd_grpc  = "protoc --go_out=. --go-grpc_out=. {0}".format(protofile)
-        compile_cmd_micro  = "protoc --proto_path=. --micro_out=. --go_out=. {0}".format(protofile)
-        subprocess.call(compile_cmd_grpc,shell=True)
-        subprocess.call(compile_cmd_micro,shell=True)
+        compile_cmd_grpc = "protoc --go_out=. --go-grpc_out=. {0}".format(
+            protofile)
+        compile_cmd_micro = "protoc --proto_path=. --micro_out=. --go_out=. {0}".format(
+            protofile)
+        subprocess.call(compile_cmd_grpc, shell=True)
+        subprocess.call(compile_cmd_micro, shell=True)
         print(compile_cmd_grpc)
         print(compile_cmd_micro)
-
-
-
 
     except getopt.GetoptError:
         print("###################################################")
@@ -62,8 +66,6 @@ def main(argv):
         print("\n")
         print("###################################################")
         sys.exit()
-
-
 
 
 if __name__ == '__main__':
