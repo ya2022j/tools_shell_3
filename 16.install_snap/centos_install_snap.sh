16fd65c6d483431767f4988f6cfbb1e41d353745
ya2022j <62124198+ya2022j@users.noreply.github.com>
2023-08-30 13:36:37 +0900

    Create centos_install_snap.sh

---
+++ 16.install_snap/centos_install_snap.sh
@@ -0,0 +1,17 @@
+
+
+yum install -y epel-release
+
+yum install -y snapd
+
+systemctl enable --now snapd.socket
+
+ln -s /var/lib/snaped/snap  /snap
+
+
+which snap
+
+snap version 
+
+sudo snap install another-redis-desktop-manager ;
+sudo snap install hello-world
